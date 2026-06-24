46e0f878bfd58344492e112eae1dd0b306b645c4
wangxiaoyou1993 <xiaoyou@mage.ai>
2026-06-23 17:34:26 -0700

    Add log search chart unit tests

--- .github/actions/chart-testing/entrypoint.sh
+++ .github/actions/chart-testing/entrypoint.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add opensearch https://opensearch-project.github.io/helm-charts
+helm plugin install https://github.com/helm-unittest/helm-unittest.git --version v0.4.1
+for chart in charts/*; do
+  if [ -d "$chart/tests" ]; then
+    helm unittest "$chart"
+  fi
+done
 ct lint --validate-maintainers=false --all --chart-dirs charts
